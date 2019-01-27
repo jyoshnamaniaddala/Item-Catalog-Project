6710cdfa9025e977c0db2ff70b332416db821f55
JYOSHNA MANI ADDALA <45555841+jyoshnamaniaddala@users.noreply.github.com>
2019-01-27 20:36:05 +0530

    database python file

---
+++ database_setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class States(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id,
+            }
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    state = relationship(States)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            }
+engine = create_engine('sqlite:///projectdatabase.db')
+
+
+Base.metadata.create_all(engine)
